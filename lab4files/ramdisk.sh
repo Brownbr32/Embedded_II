93aba99f19c3694f8ace8fa9877af53ecc53ad4b
Brock Brown <43049440+Brownbr32@users.noreply.github.com>
2020-03-09 18:50:54 -0600

    much less noob-proof, but more expert friendly

--- lab4files/ramdisk.sh
+++ lab4files/ramdisk.sh
@@ -1,1 +1,2 @@
+ export PATH=$PATH:/mnt/c/u-boot/tools        # edit this path to point to the tools in the u-boot directory
  ./u-boot/tools/mkimage -A arm -T ramdisk -c gzip -d ./ramdisk8M.image.gz uramdisk.image.gz
